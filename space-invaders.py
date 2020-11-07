4f3e1b473f5c32cc7bce51e2367c936087167152
Rafe <rafe.otaya@gmail.com>
2020-11-07 16:46:05 +0200

    Created a Bullet and a bulletstatus to control its behavior. Binded firing with space bar. Border checking and changing status.

--- space-invaders.py
+++ space-invaders.py
@@ -40,6 +40,24 @@ enemy.setposition(-200,250)
 
 enemyspeed = 2
 
+#Create the player's bullet
+bullet = turtle.Turtle()
+bullet.color("yellow")
+bullet.shape("triangle")
+bullet.penup()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(0.5, 0.5)
+bullet.hideturtle()
+
+bulletspeed = 20
+
+#Define bullet state
+#ready - ready to fire
+#fire  - bullet is firing
+bulletstate = "ready"
+
+
 #Move the player left and right
 def move_left():
     x = player.xcor()
@@ -55,10 +73,23 @@ def move_right():
         x = 280
     player.setx(x)
 
+def fire_bullet():
+    #Declare a bulletstate as a global if it needs changed
+    global bulletstate 
+    if bulletstate == "ready":
+        bulletstate = "fire"
+        #Move the bullet to just above the player
+        x = player.xcor()
+        y = player.ycor() + 10
+        bullet.setposition(x,y)
+        bullet.showturtle()
+
+
 #Create keyboard binding
 turtle.listen()
 turtle.onkey(move_left, "Left")
 turtle.onkey(move_right, "Right")
+turtle.onkey(fire_bullet, "space")
 
 # Main game loop
 while True:
@@ -76,7 +107,16 @@ while True:
         enemy.sety(enemy.ycor() - 40)
         enemyspeed *= -1
 
+    #Move the bullet
+    if bulletstate == "fire":
+        y = bullet.ycor()
+        y += bulletspeed
+        bullet.sety(y)
 
+    #Check to see if the bullet has gone to the top
+    if bullet.ycor() > 275:
+        bullet.hideturtle()
+        bulletstate = "ready"
 
 
 
